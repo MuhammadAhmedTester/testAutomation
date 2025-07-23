e6990a7d0669e49ddf9bbd517792410ec0cf1891
abu <abu.afzal@abraxas.ch>
2025-07-23 13:51:41 +0500

    small fix

--- cypress/e2e/creatinglyTests.cy.js
+++ cypress/e2e/creatinglyTests.cy.js
@@ -4,8 +4,24 @@ describe("Chart Page Automation Tests", () => {
   const chartPage = new ChartPage();
 
   before(() => {
-    chartPage.visitPlatform();
-    // chartPage.checkPageState();
+    beforeAll(() => {
+      // Visit the platform and set up clean state
+      chartPage.visitPlatform();
+      chartPage.checkPageState();
+      
+      // Check if we're in the layout section, if not create master page
+      cy.get('body').then(($body) => {
+        const hasLayoutSection = $body.find('#section1.layout-style').length > 0;
+        
+        if (!hasLayoutSection) {
+          // Create master page to get to layout section
+          chartPage.createMasterPage();
+        }
+        
+        // Analyze if chart is present in section, click on it and delete if found
+        chartPage.analyzeAndDeleteChartIfPresent();
+      });
+    });
   });
 
   describe("Happy Path Tests", {testIsolation: false}, () => {
